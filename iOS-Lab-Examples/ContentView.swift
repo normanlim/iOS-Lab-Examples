9c6e806b21175c2810297521b20c05ee1a3f39a3
Norman Lim <nlim19@my.bcit.ca>
2024-03-13 16:27:31 -0700

    Initial commit

---
+++ iOS-Lab-Examples/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS-Lab-Examples
+//
+//  Created by Norman Lim on 2024-03-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS-Lab-Examples/iOS_Lab_ExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Lab_ExamplesApp.swift
+//  iOS-Lab-Examples
+//
+//  Created by Norman Lim on 2024-03-13.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Lab_ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
